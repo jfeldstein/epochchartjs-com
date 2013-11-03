5a600d2d7a0585ed6ad164095cc99794f66777c9
Jordan <jfeldstein+git@gmail.com>
2013-11-02 17:02:13 -0700

    Display created charts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 EpochChart::Application.routes.draw do
   resources :charts, :data
 
-  match "/chart/:id/embed" => 'charts#embed', as: "embed"
+  match "/charts/:id/embed" => 'charts#embed', as: "embed"
   match "/data/:id/calendar" => 'data#calendar', as: "calendar_data"
 
   match "/how-to" => 'charts#howto', as: "howto"
